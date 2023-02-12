8d65fa14483c3b62ed70216d3b17258ba4236077
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-02-12 21:08:32 +0300

    update post and get methods

---
+++ SharpTaste/Controllers/BreakfastsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharpTaste.Contracts;
+using SharpTaste.Models;
+using SharpTaste.Services.Breakfasts;
+
+namespace SharpTaste.Controllers
+{
+    [ApiController]
+    [Route("breakfasts")]
+    public class BreakfastsController : Controller
+    {
+        private readonly IBreakfastService _breakfastService;
+
+        public BreakfastsController(IBreakfastService breakfastService)
+        {
+            _breakfastService = breakfastService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateBreakfastRequest(CreateBreakfastRequest request)
+        {
+            var breakfast = new Breakfast(
+                Guid.NewGuid(),
+                request.Name,
+                request.Description,
+                request.StartDateTime,
+                request.EndDateTime,
+                DateTime.UtcNow,
+                request.Savory,
+                request.Sweet);
+
+            // TODO save to db
+            _breakfastService.CreateBreakfast(breakfast);
+
+            var response = new BreakfastResponse(
+                breakfast.Id,
+                breakfast.Name,
+                breakfast.Description,
+                breakfast.StartDateTime,
+                breakfast.EndDateTime,
+                breakfast.LastModifiedDateTime,
+                breakfast.Savory,
+                breakfast.Sweet);
+
+            return CreatedAtAction(
+                 actionName: nameof(GetBreakfast),
+                 routeValues: new { id = breakfast.Id },
+                 value: response);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetBreakfast(Guid id)
+        {
+            Breakfast breakfast = _breakfastService.GetBreakfast(id);
+
+            var response = new BreakfastResponse(breakfast.Id,
+                breakfast.Name,
+                breakfast.Description,
+                breakfast.StartDateTime,
+                breakfast.EndDateTime,
+                breakfast.LastModifiedDateTime,
+                breakfast.Savory,
+                breakfast.Sweet);
+
+            return Ok(response);
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+    }
+}

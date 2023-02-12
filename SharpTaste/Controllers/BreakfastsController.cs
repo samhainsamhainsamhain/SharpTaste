af1c7cc62de84fe38ddf3b04cfd6cc48282b6b73
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-02-12 19:57:24 +0300

    create basic api routes

---
+++ SharpTaste/Controllers/BreakfastsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharpTaste.Contracts;
+
+namespace SharpTaste.Controllers
+{
+    [ApiController]
+    public class BreakfastsController : Controller
+    {
+        [HttpPost("/breakfasts")]
+        public IActionResult CreateBreakfastRequest(CreateBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpGet("/breakfasts/{id:guid}")]
+        public IActionResult GetBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPut("/breakfasts/{id:guid}")]
+        public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpDelete("/breakfasts/{id:guid}")]
+        public IActionResult DeleteBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+    }
+}

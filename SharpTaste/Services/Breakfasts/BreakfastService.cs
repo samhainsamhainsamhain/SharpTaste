f97c9a0bb9cd686d39c832f64b68c25aab255450
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-02-12 21:08:22 +0300

    add services for breakfasts

---
+++ SharpTaste/Services/Breakfasts/BreakfastService.cs
@@ -0,0 +1,20 @@
+﻿using SharpTaste.Models;
+
+namespace SharpTaste.Services.Breakfasts
+{
+    public class BreakfastService : IBreakfastService
+    {
+        // TODO move data storage from memory to db
+        private static readonly Dictionary<Guid, Breakfast> _breakfasts = new();
+
+        public void CreateBreakfast(Breakfast breakfast)
+        {
+        _breakfasts.Add(breakfast.Id, breakfast);
+        }
+
+        public Breakfast GetBreakfast(Guid id)
+        {
+            return _breakfasts[id];
+        }
+    }
+}
---
+++ SharpTaste/Services/Breakfasts/IBreakfastService.cs
@@ -0,0 +1,11 @@
+﻿using SharpTaste.Contracts;
+using SharpTaste.Models;
+
+namespace SharpTaste.Services.Breakfasts
+{
+    public interface IBreakfastService
+    {
+        void CreateBreakfast(Breakfast request);
+        Breakfast GetBreakfast(Guid id);
+    }
+}
